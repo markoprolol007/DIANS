63e8f8eaf3bb35367db66a6ca29949239f82f07d
Marko Poposki <129440112+markoprolol007@users.noreply.github.com>
2024-11-14 19:33:46 +0100

    main.py

--- DIANS_domasna1/main.py
+++ DIANS_domasna1/main.py
@@ -21,10 +21,8 @@ def get_data_for(code, startDate, endDate):
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        # наслови на колоните
         headers = [th.text.strip() for th in soup.find_all('th')]
 
-        # најди ги сите редови во табелата и прескокни го првиот
         column_row = soup.findAll('tr')[1:]
 
         table = soup.find('table')
@@ -32,7 +30,7 @@ def get_data_for(code, startDate, endDate):
         if table:
             rows = []
             for row in column_row:
-                row_data = row.find_all('td')  # најди ги сите полиња во редот
+                row_data = row.find_all('td')
                 if row_data:
                     individual_row_data = [pole.text.strip() for pole in row_data]
                     individual_row_data.insert(0, code)
